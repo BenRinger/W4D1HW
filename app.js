364159ee54abb21be0e454dfee4abf13b081285d
Ben <ringerben1@gmail.com>
2022-04-18 17:15:33 -0400

    first push

---
+++ app.js
@@ -0,0 +1,31 @@
+// //push in ascending order (by 1) using 'for' loop
+// var arr=[];
+// for(let i=0; i<=10; i++){
+//     arr.push(i);
+// }
+
+// //push in ascending order (by 1) using a 'while' loop
+// var arr2=[];
+// let i=0;
+// while (i<10){
+//     i++;
+//     arr2.push(i);
+// }
+
+// // push in asceding order odds 11-21 with a 'for' loop
+// var arr3 = [];
+// for(let i = 11; i <= 21; i += 2){
+//     arr3.push(i);
+// }
+
+// // push in descending order odd numbers 15-1 with a 'while' loop
+var arr4=[];
+let i=17;
+while (i>1){
+    i-=2;
+    arr4.push(i);
+}
+
+
+
+
